cdb95df99967e821c27de4e94e8bdf4c0936606b
Thiago <tgomes_2@hotmail.com>
2025-03-30 16:32:20 -0300

    feat: add Section5 to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,10 +5,11 @@ import { Section2Component } from "./section2/section2.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { Section3Component } from "./section3/section3.component";
 import { Section4Component } from "./section4/section4.component";
+import { Section5Component } from "./section5/section5.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, Section1Component, Section2Component, FooterComponent, Section3Component, Section4Component],
+  imports: [HeaderComponent, Section1Component, Section2Component, FooterComponent, Section3Component, Section4Component, Section5Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/pages/home/section5/section5.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section5',
+  imports: [],
+  templateUrl: './section5.component.html',
+  styleUrl: './section5.component.css'
+})
+export class Section5Component {
+
+}
